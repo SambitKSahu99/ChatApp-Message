212ffe945353790802b4da74d32262249894747d
Sambit Kumar Sahu <sambit.sahu@theelixrlabs.com>
2024-09-29 22:36:17 +0530

    code changes

--- src/main/java/com/elixr/ChatApp_Message/contants/UrlConstants.java
+++ src/main/java/com/elixr/ChatApp_Message/contants/UrlConstants.java
@@ -1,9 +1,9 @@
 package com.elixr.ChatApp_Message.contants;
 
 public class UrlConstants {
-    public static final String AUTH_SERVICE_URL = "https://chatapp-authservice.internal.orangecoast-52406e6a.southindia.azurecontainerapps.io";
+    public static final String AUTH_SERVICE_URL = "https://chatapp-authservice.orangecoast-52406e6a.southindia.azurecontainerapps.io";
     public static final String MESSAGE_ENDPOINT = "/message";
     public static final String UI_URL = "https://chatapp-webbrowser.orangecoast-52406e6a.southindia.azurecontainerapps.io";
-    public static final String USER_SERVICE_URL = "https://chatapp-userservice.internal.orangecoast-52406e6a.southindia.azurecontainerapps.io";
+    public static final String USER_SERVICE_URL = "https://chatapp-userservice.orangecoast-52406e6a.southindia.azurecontainerapps.io";
     public static final String VERIFY_TOKEN_ENDPOINT = "/verifyToken";
 }
