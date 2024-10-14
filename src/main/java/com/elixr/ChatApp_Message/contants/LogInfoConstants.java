004d191e9adf2a41feb3e5c0ce3f97df0d860127
Sambit Kumar Sahu <sambit.sahu@theelixrlabs.com>
2024-10-14 10:27:24 +0530

    Updated jwt validation

--- src/main/java/com/elixr/ChatApp_Message/contants/LogInfoConstants.java
+++ src/main/java/com/elixr/ChatApp_Message/contants/LogInfoConstants.java
@@ -1,8 +1,8 @@
 package com.elixr.ChatApp_Message.contants;
 
 public class LogInfoConstants {
-    public static final String CALLING_AUTH_SERVICE = "Calling auth service to verify token";
     public static final String SAVING_MESSAGE_IN_DB = "Saving message in the database";
     public static final String RETRIEVING_MESSAGES_BY_USERNAME = "Retrieving messages from database with username {}";
     public static final String SENDING_ALL_MESSAGES = "Sending all user related messages";
+    public static String Token_IS_VALID = "Token is valid";
 }
--- src/main/java/com/elixr/ChatApp_Message/contants/MessageConstants.java
+++ src/main/java/com/elixr/ChatApp_Message/contants/MessageConstants.java
@@ -1,9 +1,11 @@
 package com.elixr.ChatApp_Message.contants;
 
 public class MessageConstants {
-    public static final Object ACCESS_DENIED = "Forbidden Access Denied";
-    public static final String INVALID_TOKEN = "Invalid Token";
+    public static final String ACCESS_DENIED = "Forbidden Access Denied ";
+    public static final String INVALID_TOKEN = "Invalid Token :";
     public static final String MAXIMUM_ALLOWED_CHARACTER_MESSAGE = "Max 250 characters allowed";
     public static final String MESSAGE_INSERTION_SUCCESSFUL = "Message Saved";
     public static final String MESSAGE_NOT_FOUND = "No messages found";
+    public static final String NOT_VALID_TOKEN = "Invalid token";
+    public static final String SOME_ERROR_OCCURRED = "Some internal error occurred";
 }
